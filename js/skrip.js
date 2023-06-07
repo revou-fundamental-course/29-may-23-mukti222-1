183e529bec8d54c3e845dbd395046c73a86fb55d
mukti222 <135799527+mukti222@users.noreply.github.com>
2023-06-07 19:52:26 +0700

    menambahkan media queri

--- js/skrip.js
+++ js/skrip.js
@@ -1,20 +1,21 @@
 
-                        function validateForm() {
-                            var name =document.forms['message-form']['full-name'].value
-                            var date =document.forms['message-form']['birth-date'].value
-                            var radio =document.forms['message-form']['gender'].value
-                            var messages =document.forms['message-form']['messages'].value
-                            
-                
-                            if (name == "") {
-                                alert("nama tidak boleh kosong");
-                                return false; }
-                            // }  jika return false maka tidak akan terus kebawah
-                            
-                
-                            document.getElementById("sender-full-name").innerText=name
-                            document.getElementById("sender-birth-date").innerText=date
-                            document.getElementById("sender-gender").innerText=radio
-                            document.getElementById("sender-messages").innerText=messages
-                            return false;
-                        }
+function validateForm() {
+    var name = document.forms['message-form']['full-name'].value
+    var date = document.forms['message-form']['birth-date'].value
+    var radio = document.forms['message-form']['gender'].value
+    var messages = document.forms['message-form']['messages'].value
+
+
+    if (name == "") {
+        alert("nama tidak boleh kosong");
+        return false;
+    }
+    // }  jika return false maka tidak akan terus kebawah
+
+
+    document.getElementById("sender-full-name").innerText = name
+    document.getElementById("sender-birth-date").innerText = date
+    document.getElementById("sender-gender").innerText = radio
+    document.getElementById("sender-messages").innerText = messages
+    return false;
+}
